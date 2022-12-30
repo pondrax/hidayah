49489c6994d0a5965a36860d4aa2b2e2bc42cb93
pondra <pondras.setiawan@gmail.com>
2022-12-30 12:50:26 +0700

    add cache control tts

--- src/routes/api/tts/+server.ts
+++ src/routes/api/tts/+server.ts
@@ -2,8 +2,13 @@ import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import fs from 'fs';
 
-export const GET = (async ({ url }) => {
-  const resp = await getAudio(url.searchParams.get('q')??'')
+export const GET = (async ({ url, setHeaders }) => {
+  const resp = await getAudio(url.searchParams.get('q') ?? '')
+
+  setHeaders({
+    'age': '31536000',
+    'cache-control': 'max-age=31536000, immutable'
+  });
   return new Response(resp);
 }) satisfies RequestHandler;
 
@@ -30,7 +35,7 @@ const getAudio = async (text: string) => {
     textArray.map(async (str) => {
       const body = await fetch(`https://texttospeech.responsivevoice.org/v1/text:synthesize?text=${encodeURIComponent(str)}&lang=id&engine=g3&name=&pitch=0.5&rate=0.5&volume=1&key=kvfbSITh&gender=male`);
       // const body = await fetch(`http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=${encodeURIComponent(str)}&tl=Id`);
-     
+
       const arrayBuffer = await body.arrayBuffer();
       return Buffer.from(arrayBuffer)
     })
